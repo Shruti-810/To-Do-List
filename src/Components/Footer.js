ad3f316356be030eea240c01627593b70876437d
Shruti Makwana <shrutimak810@gmail.com>
2021-10-19 13:36:26 +0530

    First Commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import './Footer.css'
+import React, { Component } from 'react'
+
+
+const Footer=()=>{
+    return(
+        <>
+        <div id="footer">
+            <h3>Contact Us</h3>
+        </div>
+        </>
+    )
+}
+
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+const Header=()=>{
+    return(
+        <>
+               <ul>
+                   <li>Sign In</li>
+                   <li>Sign Up</li>
+               </ul>
+        </>
+    )
+}
+
+export default Header
